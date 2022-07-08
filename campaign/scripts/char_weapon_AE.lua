3963739713a2b6885fcd025ed33526dc0e890fa6
Wil Thieme <wil.thieme@protonmail.com>
2022-07-08 15:19:06 -0400

    rebase char_weapon_AE.lua

--- campaign/scripts/char_weapon_AE.lua
+++ campaign/scripts/char_weapon_AE.lua
@@ -2,42 +2,51 @@
 -- Please see the LICENSE.md file included with this distribution for
 -- attribution and copyright information.
 --
--- luacheck: globals type
+-- luacheck: globals onDamageChanged
 function onDamageChanged()
+	damageview.setValue(table.concat(aDamage, '\n+ '));
+
 	local nodeWeapon = getDatabaseNode();
-	local nodeChar = nodeWeapon.getChild('...')
+	local nodeChar = nodeWeapon.getChild("...")
 	local rActor = ActorManager.resolveActor(nodeChar);
-
-	-- ADDITION FOR ADVANCED EFFECTS
-	local sBaseAbility = 'strength';
-	if type.getValue() == 1 then sBaseAbility = 'dexterity'; end
-	-- END ADDITION FOR ADVANCED EFFECTS
-
+	
 	local aDamage = {};
-	local aDamageNodes = UtilityManager.getSortedTable(DB.getChildren(nodeWeapon, 'damagelist'));
-	for _, v in ipairs(aDamageNodes) do
-		local aDice = DB.getValue(v, 'dice', {});
-		local nMod = DB.getValue(v, 'bonus', 0);
-		local sAbility = DB.getValue(v, 'stat', '');
+	local aDamageNodes = UtilityManager.getSortedTable(DB.getChildren(nodeWeapon, "damagelist"));
+	for _,v in ipairs(aDamageNodes) do
+		local aDice = DB.getValue(v, "dice", {});
+		local nMod = DB.getValue(v, "bonus", 0);
+
+		local sAbility = DB.getValue(v, "stat", "");
 
 		-- ADDITION FOR ADVANCED EFFECTS
-		if sAbility == 'base' then sAbility = sBaseAbility; end
-		-- END ADDITION FOR ADVANCED EFFECTS
-
-		if sAbility ~= '' then
-			local nAbilityBonus = ActorManager35E.getAbilityBonus(rActor, sAbility);
-			local nMult = DB.getValue(v, 'statmult', 1);
-			if nAbilityBonus > 0 and nMult ~= 1 then nAbilityBonus = math.floor(nMult * nAbilityBonus); end
-			nMod = nMod + nAbilityBonus;
+		if sAbility == 'base' then
+			sAbility = 'strength';
+			if DB.getValue(nodeWeapon, 'type') == 1 then sAbility = 'dexterity'; end
 		end
+		-- END ADDITION FOR ADVANCED EFFECTS
+	
+		if sAbility ~= "" then
+			local nMult = DB.getValue(v, "statmult", 1);
+			local nMax = DB.getValue(v, "statmax", 0);
+			local nAbilityBonus = ActorManager35E.getAbilityBonus(rActor, sAbility);
+			if nMax > 0 then
+				nAbilityBonus = math.min(nAbilityBonus, nMax);
+			end
+			if nAbilityBonus > 0 and nMult ~= 1 then
+				nAbilityBonus = math.floor(nMult * nAbilityBonus);
+			end
+			nMod = nMod + nAbilityBonus;
+		endy
 
 		if #aDice > 0 or nMod ~= 0 then
-			local sDamage = DiceManager.convertDiceToString(DB.getValue(v, 'dice', {}), nMod);
-			local sType = DB.getValue(v, 'type', '');
-			if sType ~= '' then sDamage = sDamage .. ' ' .. sType; end
+			local sDamage = StringManager.convertDiceToString(DB.getValue(v, "dice", {}), nMod);
+			local sType = DB.getValue(v, "type", "");
+			if sType ~= "" then
+				sDamage = sDamage .. " " .. sType;
+			end
 			table.insert(aDamage, sDamage);
 		end
 	end
 
-	damageview.setValue(table.concat(aDamage, '\n+ '));
+	damageview.setValue(table.concat(aDamage, "\n+ "));
 end
