20d12f3e393f6237aadd87838fcb8dd2a75cf2c5
Ryan Hagelstrom <rhagelstrom@gmail.com>
2025-03-05 17:23:11 -0600

    Tabs

--- utility/scripts/advanced_effects_subwindow.lua
+++ utility/scripts/advanced_effects_subwindow.lua
@@ -12,6 +12,7 @@ function onInit()
     if not Session.IsHost then
         DB.addHandler(nodePath .. '.isidentified', 'onUpdate', updateVis);
     end
+    updateVis();
     onLockModeChanged();
 end
 
@@ -33,45 +34,38 @@ function updateVis()
     local bReadOnly = WindowManager.getReadOnlyState(node);
     if not bReadOnly then
         if nID == 1 then
-            WindowManager.callSafeControlsSetLockMode(self, {'effects_list_iadd'}, false);
+            WindowManager.callSafeControlsSetLockMode(self, {'advanced_effects_list_iadd'}, false);
         else
-            WindowManager.callSafeControlsSetLockMode(self, {'effects_list_iadd'}, true);
+            WindowManager.callSafeControlsSetLockMode(self, {'advanced_effects_list_iadd'}, true);
         end
     end
-    self.effects_list.updateVis()
+    self.advanced_effects_list.updateVis();
 
 end
 
 function onLockModeChanged()
     local nodeRecord = getDatabaseNode();
     local bReadOnly = WindowManager.getReadOnlyState(nodeRecord);
-    WindowManager.callSafeControlsSetLockMode(self, {'effects_list_iadd'}, bReadOnly);
-    self.effects_list.onLockModeChanged(bReadOnly);
-    if bReadOnly then
-        if self.effects_list.getWindowCount(true) == 0 then
-            parentcontrol.setVisible(false);
-        else
-            parentcontrol.setVisible(true);
-        end
-    else
-        parentcontrol.setVisible(true);
-    end
+    WindowManager.callSafeControlsSetLockMode(self, {'advanced_effects_list_iadd'}, bReadOnly);
+    self.advanced_effects_list.onLockModeChanged(bReadOnly);
 end
 
 function onDrop(_, _, draginfo)
-    if not effects_list_iadd.getLockMode() and draginfo.getType() == 'effect' then
+    if not advanced_effects_list_iadd.getLockMode() and draginfo.getType() == 'effect' then
         local nodeAdvEffect = DB.createChild(DB.getChild(getDatabaseNode(), 'effectlist'));
         local rEffect = EffectManager.decodeEffectFromText(draginfo.getStringData(), draginfo.getSecret());
-        DB.setValue(nodeAdvEffect, 'effect', 'string', rEffect.sName);
-        DB.setValue(nodeAdvEffect, 'durmod', 'number', draginfo.getNumberData());
-        if rEffect.sUnits then
-            DB.setValue(nodeAdvEffect, 'durunit', 'string', rEffect.sUnits);
-        end
-        if rEffect.nGMOnly == 1 then
-            DB.setValue(nodeAdvEffect, 'visibility', 'string', Interface.getString('item_label_effects_hide'));
-        end
-        if rEffect.sChangeState then
-            DB.setValue(nodeAdvEffect, 'changestate', 'string' ,rEffect.sChangeState);
+        if rEffect then
+            DB.setValue(nodeAdvEffect, 'effect', 'string', rEffect.sName);
+            DB.setValue(nodeAdvEffect, 'durmod', 'number', draginfo.getNumberData());
+            if rEffect.sUnits then
+                DB.setValue(nodeAdvEffect, 'durunit', 'string', rEffect.sUnits);
+            end
+            if rEffect.nGMOnly == 1 then
+                DB.setValue(nodeAdvEffect, 'visibility', 'string', Interface.getString('item_label_effects_hide'));
+            end
+            if rEffect.sChangeState then
+                DB.setValue(nodeAdvEffect, 'changestate', 'string' ,rEffect.sChangeState);
+            end
         end
     end
 end
